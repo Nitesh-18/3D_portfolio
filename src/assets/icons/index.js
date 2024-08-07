4246e83fe26ecd8d15822448c9e1b61dddc9832d
Nitesh <niteshranjankar5@gmail.com>
2024-08-08 00:13:02 +0530

    "Added new icons (postman, vercel, vscode) to assets/icons/index.js and updated linkedin.svg file"

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -26,6 +26,9 @@ import arrow from './arrow.svg'
 import contact from './contact.svg'
 import soundon from './soundon.png'
 import soundoff from './soundoff.png'
+import postman from './postman.svg'
+import vercel from './vercel.svg'
+import vscode from './vscode.svg'
 
 export {
     css,
@@ -55,5 +58,8 @@ export {
     arrow,
     contact,
     soundon,
-    soundoff
+    soundoff,
+    postman,
+    vercel,
+    vscode
 }
